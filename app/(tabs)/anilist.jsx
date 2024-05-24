c05d2b0295f8e3433c8bb08a66c6a99ec31b63d5
ashutosh0814 <ashutosh.sri14@gmail.com>
2024-05-24 22:22:35 +0530

    Updated setWatch and fixed delete functionality in watch adn watchlist

--- app/(tabs)/anilist.jsx
+++ app/(tabs)/anilist.jsx
@@ -6,7 +6,6 @@ import Watched from "../screens/Watched";
 import Watchlist from "../screens/Watchlist";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-
 const Tab = createMaterialTopTabNavigator();
 
 export default function anilist() {
@@ -15,29 +14,31 @@ export default function anilist() {
 
   return (
     <SafeAreaView className="h-full">
-    <Tab.Navigator
-      screenOptions={{
-        tabBarLabelStyle: { fontSize: 12 },
-        tabBarStyle: { backgroundColor: "#fff" },
-      }}
-    >
-      <Tab.Screen name="SearchAnime">
-        {() => (
-          <SearchAnime
-            watched={watched}
-            setWatched={setWatched}
-            watchlist={watchlist}
-            setWatchlist={setWatchlist}
-          />
-        )}
-      </Tab.Screen>
-      <Tab.Screen name="Watched">
-        {() => <Watched watched={watched} />}
-      </Tab.Screen>
-      <Tab.Screen name="Watchlist">
-        {() => <Watchlist watchlist={watchlist} />}
-      </Tab.Screen>
-    </Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarLabelStyle: { fontSize: 12 },
+          tabBarStyle: { backgroundColor: "#fff" },
+        }}
+      >
+        <Tab.Screen name="SearchAnime">
+          {() => (
+            <SearchAnime
+              watched={watched}
+              setWatched={setWatched}
+              watchlist={watchlist}
+              setWatchlist={setWatchlist}
+            />
+          )}
+        </Tab.Screen>
+        <Tab.Screen name="Watched">
+          {() => <Watched watched={watched} setWatched={setWatched} />}
+        </Tab.Screen>
+        <Tab.Screen name="Watchlist">
+          {() => (
+            <Watchlist watchlist={watchlist} setWatchlist={setWatchlist} />
+          )}
+        </Tab.Screen>
+      </Tab.Navigator>
     </SafeAreaView>
   );
 }
